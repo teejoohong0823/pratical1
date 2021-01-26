bcd503c44896d5467a7eb1de805dc297c9945ee1
teejoohong0823 <teejh-wp18@student.tarc.edu.my>
2021-01-26 09:55:48 +0800

    commit by tjh

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //make changes here
+        //1st changes
+        //2nd changes
     }
 }
